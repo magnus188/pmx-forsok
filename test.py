c4dfd142d298ed6903cd4582fdda9f5c3fd094c3
Samegener <39514919+Samegener@users.noreply.github.com>
2020-04-24 11:46:59 +0200

    fixing linebreak bug

--- test.py
+++ test.py
@@ -1,32 +1,32 @@
 import serial
 import sys,os
 
-# List of pathnames
-fileNames = ['data/3v.txt', 'data/4,5v.txt', 'data/6v.txt',
-             'data/7,5v.txt', 'data/9v.txt', 'data/12v.txt']
 
-# Establish serial communications
-ser = serial.Serial(port='COM3', baudrate=115200, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, timeout=2)
+ser = serial.Serial(port='COM3', baudrate=115200, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, timeout=5)
+try:
+    ser.isOpen()
+    print("Serial port is open")
+except:
+    print('Error opening')
+    
+    exit()
 
-# Assert serial connection is availible
-assert ser.isOpen(), "Serial connection is not availible"
-
-stringLine = ""
+value = []
 
 if(ser.isOpen()):
     try:
         while True:
-            # Read data
-            character = ser.read()
-            if (character == "."):
-                # Write data to textfile
-                with open(fileNames[0], "a") as myfile:
-                    myfile.write(str(stringLine) + '\n')
-            else:
-                stringLine += character
+            data = ser.read()
+            text = data.decode("utf-8")
+            if text != ".":
+                value.append(text)
+            if text == ".":
+                hor = "".join(value)
+                print(hor)
+                value.clear()
 
-            
-    # Catch error
+            """with open("test2.txt", "a") as myfile:
+                myfile.write(str(text) + '\n')"""
     except Exception:
         print('Error exception')
 else:
